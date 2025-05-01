6b39ac26c9bc5f75441177d45a391083534f4a0f
Jack Zheng <jackzheng274@gmail.com>
2025-04-30 20:39:35 -0700

    feat: add form data insertion and retrieval from database

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000;
 env.config();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 const db = new pg.Client({
   user: process.env.PG_USER,
@@ -22,10 +23,25 @@ app.get("/", (req, res) => {
   res.send("Test");
 });
 
+app.get("/user", (req, res) => {
+  res.render("user.ejs");
+});
+
 app.get("/reviews", async (req, res) => {
   const result = await db.query("SELECT * FROM reviews ");
 
   res.render("reviews.ejs", { reviews: result.rows });
+});
+
+app.post("/reviews", async (req, res) => {
+  const { isbn, title, date, rating, description } = req.body;
+
+  const result = await db.query(
+    "INSERT INTO reviews (isbn, title, date, rating, description) VALUES ($1, $2, $3, $4, $5)",
+    [isbn, title, date, rating, description]
+  );
+  console.log(isbn, title, date, rating, description);
+  res.redirect("/reviews");
 });
 
 app.listen(port, () => {
